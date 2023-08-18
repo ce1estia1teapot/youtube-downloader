fb603b8a70ae0e0e107a1a58f9ba499f3d0113b8
ce1estia1teapot <godincarter17@gmail.com>
2023-08-17 23:46:33 -0400

    - minor changes

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from pytube import YouTube
 import os
 
 m_ytLinks: list[str] = []
-downloadFolder = 'C:\\Users\\Carter\\Downloads\\YoutubeDownloadTest'
+downloadFolder = r'E:\YoutubeMontreal'
+
 
 def printVideoInfo(p_yt: YouTube) -> str:
     outputString = f'> [Video] Downloading {p_yt.title}\n' \
-                    f'[Title]: {p_yt.title}\n' \
-                    f'[Channel]: {p_yt.author}\n' \
-                    f'[Length]: {(p_yt.length/60).__round__(2)}m {p_yt.length%60}s\n' \
-                    f'[File Size]: {p_yt.streams.get_highest_resolution().filesize/1000000}MB\n'
+                   f'[Title]: {p_yt.title}\n' \
+                   f'[Channel]: {p_yt.author}\n' \
+                   f'[Length]: {(p_yt.length / 60).__round__(2)}m {p_yt.length % 60}s\n' \
+                   f'[File Size]: {p_yt.streams.get_highest_resolution().filesize / 1000000}MB\n'
     return outputString
+
 
 def main():
     # print('> Where should video(s) be downloaded to?')
@@ -21,7 +23,6 @@ def main():
     while True:
         inputLink = input('> ')
         m_ytLinks.append(inputLink)
-
 
         additionalLinks: str = input('> Enter an additional link? (Y/N): ')
         if additionalLinks.upper() == 'N':
@@ -35,7 +36,9 @@ def main():
             yt = YouTube(link)
             print(printVideoInfo(p_yt=yt))
 
-            yt.streams.get_highest_resolution().download(filename=f'{yt.title}.mp4', output_path=downloadFolder)
+            yt.streams.get_highest_resolution().download(
+                filename=fr'{yt.title}.mp4'.replace('*', '').replace('?', '').replace('|', ''),
+                output_path=downloadFolder)
             print('> ===== Done =====\n')
     else:
         additionalLinks: str = input('> Enter an additional link? (Y/N): ')
@@ -43,6 +46,7 @@ def main():
             pass
         else:
             main()
+
 
 if __name__ == '__main__':
     main()
